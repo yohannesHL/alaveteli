b35183a9e9fcf9dc75129866af199eb00b040a76
Graeme Porteous <graeme@rgbp.co.uk>
2020-04-18 10:13:06 +0100

    Fix transient spec failure
    Relating to testing if local requests render exceptions in development
    mode.

--- spec/integration/errors_spec.rb
+++ spec/integration/errors_spec.rb
@@ -3,20 +3,21 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "When errors occur" do
 
-  def set_consider_all_requests_local(value)
-    method = Rails.application.method(:env_config)
-    allow(Rails.application).to receive(:env_config).with(no_args) do
-      method.call.merge(
-        'action_dispatch.show_exceptions' => true,
-        'consider_all_requests_local' => value
-      )
-    end
-    @requests_local = Rails.application.config.consider_all_requests_local
-    Rails.application.config.consider_all_requests_local = value
-  end
+  def set_consider_all_requests_local(value, example)
+    app = Rails.application
+    config = app.config
+    env_config = app.env_config
 
-  def restore_consider_all_requests_local
-    Rails.application.config.consider_all_requests_local = @requests_local
+    exceptions = env_config['action_dispatch.show_exceptions']
+    consider_all_requests_local = config.consider_all_requests_local
+
+    env_config['action_dispatch.show_exceptions'] = true
+    config.consider_all_requests_local = value
+
+    example.run
+
+    env_config['action_dispatch.show_exceptions'] = exceptions
+    config.consider_all_requests_local = consider_all_requests_local
   end
 
   before(:each) do
@@ -25,13 +26,9 @@ describe "When errors occur" do
     ActionMailer::Base.deliveries = []
   end
 
-  after(:each) do
-    restore_consider_all_requests_local
-  end
-
   context 'when considering all requests local (by default all in development)' do
 
-    before(:each) { set_consider_all_requests_local(true) }
+    around(:each) { |example| set_consider_all_requests_local(true, example) }
 
     it 'should show a full trace for general errors' do
       allow(InfoRequest).to receive(:find_by_url_title!).and_raise("An example error")
@@ -44,7 +41,7 @@ describe "When errors occur" do
 
   context 'when not considering all requests local' do
 
-    before(:each) { set_consider_all_requests_local(false) }
+    around(:each) { |example| set_consider_all_requests_local(false, example) }
 
     it "should render a 404 for unrouteable URLs using the general/exception_caught template" do
       get "/frobsnasm"
